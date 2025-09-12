e1b48d5cf43034359048b60d44a41d5ba0d0afd5
Arthur Vieira <arthur1vieira2@gmail.com>
2025-09-12 21:03:32 +0000

    aprendendo classe em java

---
+++ cursoclasse.java
@@ -0,0 +1,19 @@
+
+public class cursoclasse{
+    public static void main(String[]args){
+        
+
+        var male = new person();
+        male set.name("João");
+        male set.age(12);
+
+        var female = new person();
+        female set.name("Maria");
+        female set.age(10);
+
+        System.out.printf("Male name:", male.getName());
+        System.out.printf("Male age:", male.getage());
+        System.out.printf("female name: ", female.getName());
+        System.out.printf("female", female.getage());
+    }
+}
