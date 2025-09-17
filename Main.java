a9cb7b879168fb759b22a5da04e49f3d7f51a954
Arthur Vieira <arthur1vieira2@gmail.com>
2025-09-17 23:37:07 +0000

    aula de classe finalizada

--- Main.java
+++ Main.java
@@ -2,16 +2,14 @@
 public class Main{
     
     public static void main(String[]args){
+
+        var male = new Person("João");
+        male.incAge();
+
+        var female = new Person("Maria");
+        female.incAge();
         
-        var male = new Person();
-        male.name = "João";
-        male.age = 12;
-
-        var female = new Person();
-        female.name = "Maria";
-        female.age = 11;
-
-        System.out.println("Male name: " + male.getName() + "male age: " + male.teste());
-        System.out.println("female name: " + female.getName() + "female age: " + female.getAge());
+        System.out.println("Male name: " + male.getName() + " male age: " + male.getAge());
+        System.out.println("female name: " + female.getName() + " female age: " + female.getAge());
     }
 }
--- Person.java
+++ Person.java
@@ -1,27 +1,31 @@
+import java.time.OffsetDateTime;
+
 public class Person{
 
-    public static String teste(){
-        return "teste";
-    }
-
-    private String name;
+    private final String name;
 
     private int age; 
+    
+    private int lastYearAgeInc = OffsetDateTime.now().getYear();
+
+    public Person(String name){
+        this.name = name;
+        this.age = 1;
+    }
 
     public String getName(){
         return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
     }
 
     public int getAge(){
         return age;
     }
 
-    public void setAge(int age){
-        this.age = age;
-}
+    public void incAge(){
+        if(this.lastYearAgeInc >= OffsetDateTime.now().getYear()) return;
+
+        this.age +=1;
+        this.lastYearAgeInc = OffsetDateTime.now().getYear();
+    }
 
 }
